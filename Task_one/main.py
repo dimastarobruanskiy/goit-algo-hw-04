e671dd3c00fc6e559c903123e5e380b6b7d0b221
dimastarobruanskiy <dima.starobruanskiy@gmail.com>
2026-04-08 17:22:16 +0300

    Вніс зміни в текст виводу

--- Task_one/main.py
+++ Task_one/main.py
@@ -1,5 +1,4 @@
 from salary import total_salary
 
 total, average = total_salary("salaries.txt")
-print(f"Загальна сума: {total}")
-print(f"Середня зарплата: {average}")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
