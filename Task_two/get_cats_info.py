debd26dd1093c204052894a7d455921a33ef236f
dimastarobruanskiy <dima.starobruanskiy@gmail.com>
2026-04-08 18:40:34 +0300

    Add files via upload

---
+++ Task_two/get_cats_info.py
@@ -0,0 +1,23 @@
+def get_cats_info(path):
+    cats_list = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                
+                if len(parts) == 3:
+                    cat_dict = {
+                        "id": parts[0],
+                        "name": parts[1],
+                        "age": parts[2]
+                    }
+                    cats_list.append(cat_dict)
+                    
+        return cats_list
+
+    except FileNotFoundError:
+        print(f"Помилка: Файл за шляхом '{path}' не знайдено.")
+        return []
+    except Exception as e:
+        print(f"Сталася непередбачена помилка: {e}")
+        return []
---
+++ Task_two/main.py
@@ -0,0 +1,4 @@
+from get_cats_info import get_cats_info
+
+cats_info = get_cats_info("Cats.txt")
+print(cats_info)
